f55125e94c22bbc8d3c9c4e92d98f1541edb53a9
mtoshpulatov1995@gmail.com <4377433mtDEV>
2021-05-21 11:44:28 +0500

    wqre

--- api/server.js
+++ api/server.js
@@ -7,16 +7,20 @@ require("./db/mongoose");
 app.use(cors());
 app.use(express.json());
 
+
 app.use(userRouter);
 
-if (process.env.NODE_ENV === "production") {
+
+if(process.env.NODE_ENV==="production") {
   
 }
 
-const PORT = process.env.PORT | 4000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, function () {
   console.log("Server is running on Port: " + PORT);
 });
+
 
 app.get("/", (req, res) => {
   res.send("Api is running");
